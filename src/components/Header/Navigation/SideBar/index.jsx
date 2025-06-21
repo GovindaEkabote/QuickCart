b89876a9d1af2178cf9f11f34a1aae8a69a1b349
GovindaEkbote <govindaekbote7@gmail.com>
2025-06-22 00:17:27 +0530

    Create side bar

---
+++ src/components/Header/Navigation/SideBar/index.jsx
@@ -0,0 +1,43 @@
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+
+const SideBar = ({ isOpenSideBar, closeSideBar }) => {
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={closeSideBar}>
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <Drawer open={isOpenSideBar} onClose={closeSideBar}>
+      {DrawerList}
+    </Drawer>
+  );
+};
+
+
+export default SideBar;
--- src/components/Header/Navigation/index.jsx
+++ src/components/Header/Navigation/index.jsx
@@ -1,120 +1,132 @@
 import { Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineMenu } from "react-icons/md";
 import { IoIosArrowDown } from "react-icons/io";
 import { Link } from "react-router-dom";
 import { TbTruckDelivery } from "react-icons/tb";
-
-
+import "./style.css";
+import SideBar from "./SideBar";
 
 const Navigation = () => {
+
+
+  const [isOpenSideBar, setIsOpenSideBar] = useState(false);
+  const openSideBar = () => {
+  setIsOpenSideBar(true);
+};
+
+const closeSideBar = () => {
+  setIsOpenSideBar(false);
+};
   return (
-    <nav className="py-2">
-      <div className="container flex justify-start items-center gap-8">
-        <div className="col_1 w-[20%]">
-          <Button className="!text-black w-full">
-            <MdOutlineMenu className="text-[18px]" />
-            &nbsp; Shop By Categories &nbsp;&nbsp;
-            <IoIosArrowDown className="text-[18px] ml-auto font-bold " />
-          </Button>
+    <>
+      <nav className="py-2">
+        <div className="container flex justify-start items-center gap-8">
+          <div className="col_1 w-[20%]">
+            <Button className="!text-black w-full" onClick={openSideBar}>
+              <MdOutlineMenu className="text-[18px]" />
+              &nbsp; Shop By Categories &nbsp;&nbsp;
+              <IoIosArrowDown className="text-[18px] ml-auto font-bold " />
+            </Button>
+          </div>
+
+          <div className="col_2 w-[62%] ">
+            <ul className="flex items-center">
+              <li className="list-none">
+                <Link
+                  to="/"
+                  className="link transition text-[14px] uppercase font-[500]  "
+                >
+                  <Button className="link transition !text-black uppercase">
+                    HOME
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link
+                  to="/"
+                  className="link transition text-[14px] uppercase font-[500]"
+                >
+                  <Button className="link transition !text-black uppercase">
+                    Electronics
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link
+                  to="/"
+                  className="link transition text-[14px] uppercase font-[500]"
+                >
+                  <Button className="link transition !text-black uppercase">
+                    Footwares
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link
+                  to="/"
+                  className="link transition text-[14px] uppercase font-[500]"
+                >
+                  <Button className="link transition !text-black uppercase">
+                    kids
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link
+                  to="/"
+                  className="link transition text-[14px] uppercase font-[500]"
+                >
+                  <Button className="link transition !text-black uppercase">
+                    accessories
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link
+                  to="/"
+                  className="link transition text-[14px] uppercase font-[500]"
+                >
+                  <Button className="link transition !text-black uppercase">
+                    groceries
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link
+                  to="/"
+                  className="link transition text-[14px] uppercase font-[500]"
+                >
+                  <Button className="link transition !text-black uppercase">
+                    books
+                  </Button>
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link
+                  to="/"
+                  className="link transition text-[14px] uppercase font-[500]"
+                >
+                  <Button className="link transition !text-black uppercase">
+                    jewellery
+                  </Button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col_3 w-[18%]">
+            <p className="text-[13px] uppercase font-[450] flex items-center">
+              free delivery above Rs.500 &nbsp;
+              <TbTruckDelivery className="text-[25px] ml-auto font-bold" />
+            </p>
+          </div>
         </div>
-        <div className="col_2 w-[62%] ">
-          <ul className="flex items-center ">
-            <li className="list-none">
-              <Link
-                to="/"
-                className="link transition text-[14px] uppercase font-[500]"
-              >
-              <Button className="link transition !text-black uppercase">
-                HOME
-              </Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link
-                to="/"
-                className="link transition text-[14px] uppercase font-[500]"
-              >
-              <Button className="link transition !text-black uppercase">
-                Electronics
+      </nav>
 
-              </Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link
-                to="/"
-                className="link transition text-[14px] uppercase font-[500]"
-              >
-              <Button className="link transition !text-black uppercase">
-                Footwares
-
-              </Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link
-                to="/"
-                className="link transition text-[14px] uppercase font-[500]"
-              >
-              <Button className="link transition !text-black uppercase">
-                kids
-
-              </Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link
-                to="/"
-                className="link transition text-[14px] uppercase font-[500]"
-              >
-              <Button className="link transition !text-black uppercase">
-                accessories
-
-              </Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link
-                to="/"
-                className="link transition text-[14px] uppercase font-[500]"
-              >
-              <Button className="link transition !text-black uppercase">
-                groceries
-
-              </Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link
-                to="/"
-                className="link transition text-[14px] uppercase font-[500]"
-              >
-              <Button className="link transition !text-black uppercase">
-                books
-
-              </Button>
-              </Link>
-            </li>
-            <li className="list-none">
-              <Link
-                to="/"
-                className="link transition text-[14px] uppercase font-[500]"
-              >
-              <Button className="link transition !text-black uppercase">
-                jewellery
-
-              </Button>
-              </Link>
-            </li>
-           
-          </ul>
-        </div>
-        <div className="col_3 w-[18%]">
-          <p className="text-[13px] uppercase font-[450] flex items-center">free delivery above Rs.500 &nbsp;<TbTruckDelivery className="text-[25px] ml-auto font-bold"/></p>
-        </div>
-      </div>
-    </nav>
+      {/* Side Bar Component */}
+      <SideBar isOpenSideBar={isOpenSideBar} closeSideBar={closeSideBar}/>
+    </>
   );
 };
 
