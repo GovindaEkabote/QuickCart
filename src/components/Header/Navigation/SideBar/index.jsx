c93e854925201f17dbb81b952843a6d99709a1f9
GovindaEkbote <govindaekbote7@gmail.com>
2025-06-22 01:06:33 +0530

    update side bar

--- src/components/Header/Navigation/SideBar/index.jsx
+++ src/components/Header/Navigation/SideBar/index.jsx
@@ -1,34 +1,33 @@
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
-import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
+import { IoIosClose } from "react-icons/io";
+import { FaRegPlusSquare } from "react-icons/fa";
 
 const SideBar = ({ isOpenSideBar, closeSideBar }) => {
   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={closeSideBar}>
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+    <Box sx={{ width: 250 }} role="presentation" >
+      
+        {" "}
+        <h3 className="p-4 text-[15px] uppercase font-black flex justify-center">
+          Shop By Categories{" "} <IoIosClose  className=" !ml-4 cursor-pointer text-red-900 text-[25px]" onClick={closeSideBar}/>
+        </h3>
+     
       <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+
+      <div className="scroll">
+        <ul className="w-full">
+          <li className="list-none flex items-center relative">
+            <Button className="w-full text-left !text-[18px] !justify-start !text-black !px-2 ">FAshion</Button>
+            <FaRegPlusSquare className="absolute cursor-pointer text-[18px] top-[12px] right-[15px]" />
+
+            <ul>
+              
+            </ul>
+          </li>
+        </ul>
+      </div>
     </Box>
   );
 
@@ -38,6 +37,5 @@ const SideBar = ({ isOpenSideBar, closeSideBar }) => {
     </Drawer>
   );
 };
-
 
 export default SideBar;
