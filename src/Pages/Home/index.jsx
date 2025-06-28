f6e7c8d114465340ba2f4a4a7884686e51dc87f0
GovindaEkbote <govindaekbote7@gmail.com>
2025-06-28 18:19:07 +0530

    add scroll bar by using material UI to show all categories

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HomeSliders from '../../components/HomeSliders'
 import Categories from '../../components/Categories'
 import { TbTruckDelivery } from "react-icons/tb";
 import Banners from '../../components/Banners';
-
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box'; 
 
 const Home = () => {
+   const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
    <>
     <HomeSliders />
     <Categories />
     <section className='py-16 bg-white '>
       <div className='container'>
-        <div className='freeShipping !mb-5 w-full rounded-md p-4 border-2 border-amber-700 flex items-center justify-between'>
+        <div className='freeShipping !mb-5 w-[80%] !m-auto rounded-md p-4 border-2 border-amber-700 flex items-center justify-between'>
             <div className='col1 flex items-center'>
               <p className='text-2xl uppercase font-[500]'>Free Shipping</p> 
                <TbTruckDelivery className='!ml-1 text-4xl'/>
@@ -27,6 +34,42 @@ const Home = () => {
         <Banners />
       </div>
     </section>
+
+      <section className='bg-white'>
+        <div className='container !mt-8'>
+          <div className='flex items-center justify-between py-5'>
+            <div className='leftSection w-[30%]'>
+              <h3 className='uppercase font-[600] text-[20px]'>Popular Products</h3>
+              <h3 className='font-[400] text-[14px]'>Limited-time offers available until the end of March.</h3>
+            </div>
+            <div className='rightSection w-[70%]'>
+              <Box sx={{ maxWidth: { xs: 600, sm: 1200 }, bgcolor: 'background.paper' }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="scrollable"
+        scrollButtons
+        allowScrollButtonsMobile
+        aria-label="scrollable force tabs example"
+      >
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+        <Tab label="Item Four" />
+        <Tab label="Item Five" />
+        <Tab label="Item Six" />
+        <Tab label="Item Seven" />
+        <Tab label="Item 8" />
+        <Tab label="Item 9" />
+        <Tab label="Item 10" />
+        <Tab label="Item 11" />
+        <Tab label="Item 12" />
+      </Tabs>
+    </Box>
+            </div>
+          </div>
+        </div>
+      </section>
     <br/>
     <br/>
     <br/>
--- src/components/BannerBox/index.jsx
+++ src/components/BannerBox/index.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import bannerData from '../../assets/banner/banner';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
+import { Link } from 'react-router-dom';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
 const BannerBox = (props) => {
-
-
   return (
-    <div className="py-5 w-full">
+    <div className="py-5 w-full overflow-hidden box bannerBox">
       <Swiper
         spaceBetween={20}
         slidesPerView={props.item} // Mobile defaul
@@ -20,12 +19,14 @@ const BannerBox = (props) => {
         className="w-full mx-auto"
       >
         {bannerData.imagesArray.map((img, index) => (
-          <SwiperSlide key={index}>
-            <img
-              src={img}
-              alt={`Banner ${index + 1}`}
-              className="w-full h-[200px] object-cover rounded-md"
-            />
+          <SwiperSlide key={index} className="group">
+            <Link to="/">
+              <img
+                src={img}
+                alt={`Banner ${index + 1}`}
+                className="w-full h-[200px] object-cover rounded-md transition-all duration-300 ease-in-out group-hover:scale-102"
+              />
+            </Link>
           </SwiperSlide>
         ))}
       </Swiper>
